0d669fefbe637b1336accb8d444517f44ede6e59
shubham <shubham19k@gmail.com>
2024-06-23 22:41:13 +0530

    maven update

--- src/main/java/io/current_affairs/config/WebConfig.java
+++ src/main/java/io/current_affairs/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+//@Slf4j
 public class WebConfig implements WebMvcConfigurer {
 
     @Bean
@@ -13,6 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+//            	log.info("aaa");
+            	System.out.println("inside config" + registry);
                 registry.addMapping("/**")
                         .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
