7262745bc3401992041068652310a1134d0f9e67
shubham <shubham19k@gmail.com>
2024-06-25 22:04:22 +0530

    added @CrossOrigin

--- src/main/java/io/current_affairs/Precise/HuggingFaceController.java
+++ src/main/java/io/current_affairs/Precise/HuggingFaceController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins="*")
 public class HuggingFaceController {
 	
 	
